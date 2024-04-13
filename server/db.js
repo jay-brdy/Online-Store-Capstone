26d325a7a8fd263fac9004910dfd673f7ea8cd63
jay-brdy <jaysirit@gmail.com>
2024-04-13 00:44:50 -0500

    changed database name

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_online_store_db');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/jays_fishing_market_db');
 const uuid = require('uuid');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');

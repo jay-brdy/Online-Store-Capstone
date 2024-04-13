b0d6038517507e0818b2988bb7858391b4933c4b
jay-brdy <jaysirit@gmail.com>
2024-04-13 01:56:11 -0500

    testing via local

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <>
-      <h1>Jay's Online Store</h1>
+      <h1>Jay's Fishing Market</h1>
       
       <Router>
         <Navigations token={token} handleLogout={handleLogout} userId={userId} />
@@ -49,4 +49,4 @@ function App() {
 }
 
 export default App;
-export const API_URL = "https://jays-fishing-market.onrender.com/";
+export const API_URL = "https://jays-fishing-market.onrender.com";
